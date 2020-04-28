bd7a8bd358d57d6f7f15326011d9fe96c8290e64
anadi-mohan <50401653+anadi-mohan@users.noreply.github.com>
2020-04-28 10:23:20 +0530

    bugs fixed

---
+++ Chebyshev_Type_I_BSF.m
@@ -0,0 +1,50 @@
+
+clc;clear all;close all;
+f1=input("Enter the lower stop-band frequency");
+f2=input("Enetr the higher stop-band frequency");
+fu=input("Enter the higher pass-band frequency");
+fl=input("Enter the lower pass-band frequency");
+pr=input("Enter the pass-band ripple");
+sa=input("Enter the stop-band attenuation");
+fs=2000;
+T=1/fs;
+
+w1=2*pi*f1/fs;
+w2=2*pi*f2/fs;
+wu=2*pi*fu/fs;
+wl=2*pi*fl/fs;
+
+o1=(2/T)*(tan(w1/2))
+o2=(2/T)*(tan(w2/2))
+ou=(2/T)*(tan(wu/2))
+ol=(2/T)*(tan(wl/2))
+
+x=(o1*(ou-ol))/(-o1^2+ou*ol);
+y=(o2*(ou-ol))/(-o2^2+ou*ol);
+Or=min(abs(x),abs(y))
+
+ep=sqrt(10^(pr/10)-1);
+A=10^(sa/20);
+g=sqrt(((A^2)-1)/ep^2);
+
+N=log(g+sqrt(g^2+1))/log(Or+sqrt(Or^2+1))
+N=ceil(N)
+
+s=tf('s');
+hsde=1;
+for k=1:N
+	sigmak=-sinh((1/N)*asinh(1/ep))*sin((2*k-1)*pi/(2*N));
+	omegak=cosh((1/N)*asinh(1/ep))*cos((2*k-1)*pi/(2*N));
+	sk(k)=sigmak+j*omegak
+	hsde=hsde*(s-sk(k));
+end
+disp('Roots');
+disp(sk);
+b0=real(hsde.Numerator{1}(1,N+1));
+if(mod(N,2)==0)
+    k0=b0/sqrt(ep^2+1);
+else
+    k0=b0;
+end
+Hnormalised=tf([0 k0],real(hsde.Numerator{1}(1,:)));
+display(Hnormalised);
